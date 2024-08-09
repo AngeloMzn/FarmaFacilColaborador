66b81400da9aed502e1404c5af6b600a16db358b
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2024-08-08 21:20:06 -0400

    [NEW] Finished login and signup

--- farmafacil_colaborador/lib/main.dart
+++ farmafacil_colaborador/lib/main.dart
@@ -1,22 +1,36 @@
+import 'package:farmafacil_colaborador/screens/home_screen.dart';
 import 'package:farmafacil_colaborador/theme/application_theme.dart';
+import 'package:farmafacil_colaborador/utils/login_cookie.dart';
 import 'package:flutter/material.dart';
+import 'package:farmafacil_colaborador/screens/login_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
-  runApp(const FarmaFacilColab());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  bool? loggedIn = await LoginCookie.getLoginState();
+  runApp(FarmaFacilColaborador(loggedIn: loggedIn ?? false));
 }
 
-class FarmaFacilColab extends StatelessWidget {
-  const FarmaFacilColab({super.key});
+class FarmaFacilColaborador extends StatelessWidget {
+  final bool loggedIn;
+  const FarmaFacilColaborador({super.key, required this.loggedIn});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FarmaFacil - Colaboradores',
+      title: 'FarmaFacil - Colaborador',
+      themeMode: ThemeMode.light,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("pt"),
+      ],
       theme: ApplicationThemes.defaultTheme,
-      home: const Scaffold(
-        body: Center(
-          child: Text("Hello world."),
-        ),
+      home: Scaffold(
+        body: loggedIn? const HomeScreen() : const LoginScreen(),
       ),
     );
   }
